e79b02b840bfcebe0f23122ca8f9679d51609dc9
Jhones Michael <82251374+jhoneshark@users.noreply.github.com>
2023-10-31 22:52:31 -0300

    exibindo categorias

--- src/telas/Home/index.js
+++ src/telas/Home/index.js
@@ -1,11 +1,36 @@
 import React, { useState } from "react";
-import { TouchableOpacity, SafeAreaView, StyleSheet, View, Text, Modal } from "react-native";
+import {
+  TouchableOpacity,
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  Modal,
+  ScrollView,
+} from "react-native";
 import Despesas from "./components/Despesas";
 import Topo from "./components/Topo";
 import { TextInput } from "react-native";
+import { categories } from "../../utils/categories";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import CurrencyInput from "react-native-currency-input";
 
 export default function HomePage() {
   const [modalVisible, setModalVisible] = useState(false);
+  const [categorias, setCategorias] = useState({
+    key: "0",
+    name: "Selecione uma categoria",
+    icon: "timer-sand-empty",
+    color: "#000000",
+  });
+  const [categoriaVisible, setCategoriaVisible] = useState(false);
+  const [valor, setValor] = useState("");
+
+  const handlecategoria = (categoria) => {
+    setCategorias(categoria);
+    setCategoriaVisible(false);
+  };
+
   return (
     <View style={estilos.tela}>
       <SafeAreaView>
@@ -27,8 +52,44 @@ export default function HomePage() {
       >
         <View style={estilos.centeredView}>
           <View style={estilos.modalView}>
-            <TextInput style={estilos.input} placeholder="Nome" />
-            <TextInput style={estilos.input} placeholder="Valor" />
+            <TouchableOpacity
+              style={estilos.buttonCategoria}
+              onPress={() => setCategoriaVisible(true)}
+            >
+              <Icon
+                name={categorias.icon}
+                size={30}
+                color={categorias.color}
+                style={estilos.icon}
+              />
+              <Text>{categorias.name}</Text>
+            </TouchableOpacity>
+
+            <CurrencyInput
+              style={estilos.input}
+              value={valor}
+              onChangeValue={setValor}
+              prefix="R$"
+              delimiter="."
+              separator=","
+              precision={2}
+              minValue={0}
+            />
+            {categoriaVisible && (
+              <ScrollView>
+                {categories.map((item) => (
+                  <TouchableOpacity
+                    style={estilos.optionsCategories}
+                    key={item.key}
+                    onPress={() => handlecategoria(item)}
+                  >
+                    <Icon name={item.icon} size={30} color={item.color} style={estilos.icon} />
+                    <Text>{item.name}</Text>
+                  </TouchableOpacity>
+                ))}
+              </ScrollView>
+            )}
+
             <TouchableOpacity
               style={estilos.buttonStyle}
               onPress={() => setModalVisible(!modalVisible)}
@@ -43,12 +104,35 @@ export default function HomePage() {
 }
 
 const estilos = StyleSheet.create({
+  optionsCategories: {
+    gap: 10,
+    flexDirection: "row",
+    backgroundColor: "#dbdbdb",
+    marginHorizontal: 10,
+    borderRadius: 6,
+    color: "#000000",
+    padding: 15,
+    alignItems: "center",
+    marginTop: 5,
+    marginBottom: 5,
+  },
+  buttonCategoria: {
+    gap: 10,
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    marginHorizontal: 10,
+    borderRadius: 6,
+    color: "#000000",
+    padding: 15,
+    alignItems: "center",
+  },
   centeredView: {
+    paddingTop: 10,
     justifyContent: "center",
     alignItems: "center",
     marginTop: 22,
     width: "100%",
-    height: "80%",
+    height: "100%",
     backgroundColor: "#000000",
     marginTop: 50,
   },
